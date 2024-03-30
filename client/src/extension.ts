31673f1c29a2908388c3b892de4cc8cc304e553c
hscheue <hscheue@gmail.com>
2024-03-29 22:04:38 -0400

    simple client

---
+++ client/src/extension.ts
@@ -0,0 +1,32 @@
+import * as path from "path";
+import { ExtensionContext } from "vscode";
+import { LanguageClient, TransportKind } from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js")
+  );
+
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    {
+      module: serverModule,
+      transport: TransportKind.ipc,
+    },
+    {
+      documentSelector: [{ scheme: "file", language: "plaintext" }],
+    }
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
