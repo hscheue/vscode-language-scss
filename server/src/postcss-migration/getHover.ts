278543721c7e68cca2530193e9ebd3d8d49c4a85
hscheue <hscheue@gmail.com>
2024-04-19 21:40:26 -0400

    add hover just using existing completions function to match found label

--- server/src/postcss-migration/getHover.ts
+++ server/src/postcss-migration/getHover.ts
@@ -1,15 +1,28 @@
-import { Hover } from "vscode-css-languageservice";
+import { Hover, MarkupKind } from "vscode-css-languageservice";
 import { getDocument } from "./documents";
 import { Root, parse } from "postcss";
 import { HoverParams } from "vscode-languageserver";
+import { getCompletions } from "./getCompletions";
 
 export function getHover(hover: HoverParams): Hover | null {
   const doc = getDocument(hover.textDocument.uri);
   if (!doc) return null;
   const root = parse(doc.getText());
   const value = getHoveredSymbolName(root, hover);
+  const completions = getCompletions(hover.textDocument.uri);
 
-  return null;
+  const completion = completions.find((c) => c.label === value);
+
+  if (!completion) return null;
+
+  return {
+    contents: {
+      value: ["```scss", `${completion.label}`, "```"]
+        .filter(Boolean)
+        .join("\n"),
+      kind: MarkupKind.Markdown,
+    },
+  };
 }
 
 function getHoveredSymbolName(root: Root, hover: HoverParams): string {
@@ -34,17 +47,5 @@ function getHoveredSymbolName(root: Root, hover: HoverParams): string {
 }
 
 /*
-return {
-  contents: {
-    value: [
-      "```scss",
-      `${name} ${symbol?.value ?? ""}`,
-      "```",
-      `${symbol?.doc?.description ?? ""}`,
-    ]
-      .filter(Boolean)
-      .join("\n"),
-    kind: MarkupKind.Markdown,
-  },
-};
+return 
 */
