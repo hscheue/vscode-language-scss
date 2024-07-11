1bf0f9a8c8a44ce759bcf02dc8f5eb87fe39b1c2
hscheue <hscheue@gmail.com>
2024-07-11 08:46:41 -0400

    cleanup variable diagnostic

--- server/src/_diagnostics/validateDocument.ts
+++ server/src/_diagnostics/validateDocument.ts
@@ -9,7 +9,7 @@ import {
   getThemeSrc,
 } from "../_shared/settings";
 import { MixinDiagnostic, VariableDiagnostic } from "../_commands/quickFix";
-import { Rule } from "postcss";
+import { Declaration, Rule } from "postcss";
 
 function _addDiagnostic(
   uri: string,
@@ -24,40 +24,7 @@ function _addDiagnostic(
 
     root.walk((node) => {
       if (node.type === "decl") {
-        const themeVariable = theme[node.value];
-
-        if (themeVariable) {
-          const range = convertRange(node.rangeBy({ word: node.value }));
-          if (!range) return;
-
-          if (spacingPrefix && themeVariable.startsWith(spacingPrefix)) {
-            if (
-              node.prop.startsWith("padding") ||
-              node.prop.startsWith("margin") ||
-              node.prop.endsWith("gap")
-            ) {
-              sendVariableDiagnostic(diagnostics, themeVariable, range);
-            }
-          } else {
-            sendVariableDiagnostic(diagnostics, themeVariable, range);
-          }
-        } else {
-          // partial theme value validation just for hex colors
-          // i.e. border: 1px solid $color
-          const value = /#([a-f0-9])+/i.exec(node.value)?.[0];
-          if (value && theme[value]) {
-            const prop = theme[value];
-            const range = convertRange(node.rangeBy({ word: value }));
-
-            diagnostics.push({
-              severity: DiagnosticSeverity.Error,
-              message: `${prop} exists in theme file`,
-              source: "vscode-language-scss",
-              data: VariableDiagnostic.create(range, prop),
-              range,
-            });
-          }
-        }
+        simpleVariableDiagnostic(theme, node, diagnostics, spacingPrefix);
       }
     });
 
@@ -133,6 +100,48 @@ function simpleMixinDiagnostic(
             return convertRange(node.rangeBy({ word }));
           })
         ),
+        range,
+      });
+    }
+  }
+}
+
+function simpleVariableDiagnostic(
+  theme: Record<string, string>,
+  node: Declaration,
+  diagnostics: Diagnostic[],
+  spacingPrefix: string | undefined
+) {
+  const themeVariable = theme[node.value];
+
+  if (themeVariable) {
+    const range = convertRange(node.rangeBy({ word: node.value }));
+    if (!range) return;
+
+    if (spacingPrefix && themeVariable.startsWith(spacingPrefix)) {
+      if (
+        node.prop.startsWith("padding") ||
+        node.prop.startsWith("margin") ||
+        node.prop.endsWith("gap")
+      ) {
+        sendVariableDiagnostic(diagnostics, themeVariable, range);
+      }
+    } else {
+      sendVariableDiagnostic(diagnostics, themeVariable, range);
+    }
+  } else {
+    // partial theme value validation just for hex colors
+    // i.e. border: 1px solid $color
+    const value = /#([a-f0-9])+/i.exec(node.value)?.[0];
+    if (value && theme[value]) {
+      const prop = theme[value];
+      const range = convertRange(node.rangeBy({ word: value }));
+
+      diagnostics.push({
+        severity: DiagnosticSeverity.Error,
+        message: `${prop} exists in theme file`,
+        source: "vscode-language-scss",
+        data: VariableDiagnostic.create(range, prop),
         range,
       });
     }
